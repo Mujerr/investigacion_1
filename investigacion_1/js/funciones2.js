601ef016226df0ca19af1634ce70763c4da3d350
Martalo <91905695+Mujerr@users.noreply.github.com>
2022-02-07 09:26:49 +0100

    Add files via upload

---
+++ investigacion_1/js/funciones2.js
@@ -0,0 +1,4 @@
+function cargarPagina()
+{
+	document.getElementsByTagName('body')[0].innerHTML = "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum unde mollitia accusamus, itaque, odit, eaque soluta nisi in possimus non doloremque! Expedita excepturi architecto magnam magni accusamus laborum, nihil suscipit.</p>";
+}
